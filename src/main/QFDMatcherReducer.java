d3e8039fb1a241146df0c8417fd732af6069b82f
laiwei <laiwei@berkeley.edu>
2016-04-22 15:43:47 -0700

    using arralist to save iterable for using more times

--- src/main/QFDMatcherReducer.java
+++ src/main/QFDMatcherReducer.java
@@ -21,18 +21,24 @@ public class QFDMatcherReducer extends Reducer<IntWritable, WebTrafficRecord, Re
         // One thing to really remember, the Iterable element passed
         // from hadoop are designed as READ ONCE data, you will
         // probably want to copy that to some other data structure if
-        // you want to iterate mutliple times over the data.  ????????? what is this??data structure?
+        // you want to iterate mutliple times over the data.  
+
+        ArrayList<WebTrafficRecord> myArray = new ArrayList<WebTrafficRecord>();
 
         for(int i=0; i<values.length; i++)
         {
+            myArray.add(values[i]);
             //if it's a request:
-            if(values[i].getUserName==Null)
+            if(myArray.get(i).getUserName==Null)
             {
-                WebTrafficRecord request=values[i];
-                for(int j=0; j<values.length; j++)
+                WebTrafficRecord request=myArray.get(i);
+                for(int j=0; j<myArray.size(); j++)
                 {
-                    if(values[j].getCookie()==Null  && (values[j].getTimestamp()<=request.getTimestamp()-10 || request.getTimestamp()<=values[j].getTimestamp()-10) && i!=j)
-                    ctxt.write( RequestReplyMatch( request, values[j]) , NullWriteable);
+                    WebTrafficRecord reply=myArray.get(j);
+                    if(reply.getCookie()==Null  && (reply.getTimestamp()<=request.getTimestamp()-10 || request.getTimestamp()<=reply.getTimestamp()-10) && i!=j)
+                    {
+                     ctxt.write( RequestReplyMatch( request, reply) , NullWriteable);
+                    }
                 }
         }
         }
